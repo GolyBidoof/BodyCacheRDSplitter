d05b30287459109f464a819043226ddcd3ca1544
szymbar15 <szymbar1513@gmail.com>
2015-01-01 21:08:45 +0100

    Let's commit this!

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace IDKwhyamidoingthisbutifeellikeit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.AmountBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import BodyCache_rd.bin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(41, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 13);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "BodyCache_rd.bin splitter by Szymbar15";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(54, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(123, 13);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Amount of themes in file:";
+            // 
+            // AmountBox
+            // 
+            this.AmountBox.BackColor = System.Drawing.SystemColors.Control;
+            this.AmountBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AmountBox.Location = new System.Drawing.Point(183, 70);
+            this.AmountBox.Name = "AmountBox";
+            this.AmountBox.Size = new System.Drawing.Size(25, 13);
+            this.AmountBox.TabIndex = 3;
+            this.AmountBox.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save body_LZ.bins";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "BodyCache_rd.bin";
+            this.openFileDialog1.Filter = "BodyCache File|BodyCache_rd.bin|All files|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Body_LZ.bin files|*bin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 139);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.AmountBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Awesomest Splitter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox AmountBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IDKwhyamidoingthisbutifeellikeit
+{
+    public partial class Form1 : Form
+    {
+        byte[] bytes;
+        int size = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    bytes = File.ReadAllBytes(file);
+                    size = bytes.Length;
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("You didn't import any file.");
+                }
+
+            }
+            if (size % 1376256 == 0)
+            {
+                AmountBox.Text = (size / 1376256).ToString();
+            }
+            else
+            {
+                MessageBox.Show("You didn't import a valid BodyCache_rd.bin file.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (size != -1)
+            {
+                for (int i = 0; i < int.Parse(AmountBox.Text); i++)
+                {
+                    byte[] save = new byte[1376256];
+                    Array.Copy(bytes, 1376256 * i, save, 0, 1376256);
+                    saveFileDialog1.FileName = i+1 + ".bin";
+                    DialogResult resultsave = saveFileDialog1.ShowDialog(); // Show the dialog.
+                    if (resultsave == DialogResult.OK) // Test result.
+                    {
+                        string file = saveFileDialog1.FileName;
+                        try
+                        {
+                            File.WriteAllBytes(file, save);
+                        }
+                        catch (IOException)
+                        {
+                            MessageBox.Show("You didn't save any file.");
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
